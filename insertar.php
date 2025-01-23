1de9b0f3b20cb9e5e7a825ced1320b8ede2ed425
MDR57 <diego.larrabeiti03@somo.eus>
2025-01-23 08:11:03 +0100

    Update insertar.php

--- insertar.php
+++ insertar.php
@@ -12,7 +12,7 @@
 
     // Crear la consulta de inserción
     $insertar = "INSERT INTO usuarios(nombre, apellido1, apellido2, direccion, telefono, dni, preferencias)
-                 VALUES('$nombre', '$apellido1', '$apellido2', '$direccion', '$telefono', '$dni', '$preferencias')";
+                 VALUES('$nombre', ,'$apellido1', '$apellido2', '$direccion', '$telefono', '$dni', '$preferencias')";
 
     // Ejecutar la consulta
     $resultado = mysqli_query($conexion, $insertar);
